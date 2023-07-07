ae8644d58c6ff30a70394a3204fbd3e10766c4a8
Omar Kobrosli <okobrosli2@gmail.com>
2023-07-07 16:39:23 +0300

    finalized answers

--- assignment_01_Omar_Kobrosli/IP.py
+++ assignment_01_Omar_Kobrosli/IP.py
@@ -3,16 +3,18 @@ def checkIP():
     IP = input("Enter IP address: ")
     split = IP.split(".")
     number = 0
+    counter = 0
+    
     if(len(split) != 4):
         print("Invalid IP address.")
     else:
         for octet in split:
-            counter = 0
             number = int(octet) 
             if (number < 0 or number > 255) or (len(octet) > 1 and octet[0] == "0"):
                 print("Invalid IP addresss.")
-                counter += 1
                 break
+            else:
+                counter += 1
             if counter == 4:
                 print("Valid IP address.")
 
